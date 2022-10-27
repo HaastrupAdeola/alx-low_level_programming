92d1e7a36b2b706321294b26607b8b94acd43f22
HaastrupAdeola <haastrupadeola574@gmail.com>
2022-10-27 04:33:51 -0700

    task 2 resolve

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _strncopy - copys a string with n
+ * _strncpy - copys a string with n
  * @dest: copy to
  * @src: copy from
  * @n: number of char to be copied
  * Return: dest
  */
-char *_strncopy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
