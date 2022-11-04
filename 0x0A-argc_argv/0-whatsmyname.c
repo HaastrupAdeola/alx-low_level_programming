275d9d868facec708a02d767a7e5b664451ff26b
HaastrupAdeola <haastrupadeola574@gmail.com>
2022-11-04 03:24:20 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include<unistd.h>
+#include "main.h"
+
+/**
+ * main - Print the name of the program
+ * @argc: Count argumnets
+ * @argv: Arguments
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+
+return (0);
+}
